3b2e8588afac8b2aa6668c648cd3fe227a8fd1d3
kairlec666666666666 <44259087+kairlec@users.noreply.github.com>
2020-01-10 13:40:14 +0800

    create

---
+++ AutoBackup/POJO/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AutoBackup.POJO
+{
+    class Config
+    {
+        [JsonPropertyName("GlobalPath")]
+        public string GlobalPath { get; set; }
+
+    }
+}
+
